247f82cf33f3c5d1e758a51da6761b7a1bdbb467
Mohamed Aichouri <maichour@calpoly.edu>
2019-12-06 10:33:32 -0800

    getPopularity added

--- src/com/company/Rooms.java
+++ src/com/company/Rooms.java
@@ -1,13 +1,16 @@
 package com.company;
 
+import java.sql.*;
+
 public class Rooms {
-    public String RoomCode;
-    public String RoomName;
-    public int Beds;
-    public String BedType;
-    public int MaxOcc;
-    public float basePrice;
-    public String decor;
+    private String RoomCode;
+    private String RoomName;
+    private int Beds;
+    private String BedType;
+    private int MaxOcc;
+    private float basePrice;
+    private String decor;
+    private float popularity;
 
     public Rooms(String roomCode) {
         this.RoomCode = roomCode;
@@ -17,6 +20,7 @@ public class Rooms {
         this.MaxOcc = 0;
         this.basePrice = 0;
         this.decor = "";
+        this.popularity = 0;
     }
 
     public Rooms(String roomCode, String roomName, int roomBeds, String bedType, int maxOccu,
@@ -28,6 +32,7 @@ public class Rooms {
         this.MaxOcc = maxOccu;
         this.basePrice = basePrice;
         this.decor = decor;
+        this.popularity = 0;
     }
 
     public String getRoomCode() {
@@ -56,5 +61,39 @@ public class Rooms {
 
     public String getDecor() {
         return decor;
+    }
+
+    public float getPopularity(){
+        if(this.popularity != 0)
+        {
+            return this.popularity;
+        }
+        else {
+            String sql = "select roomName, (Sum(DATEDIFF(checkout,checkin))/180) as popular" +
+                    "from rooms join reservations res on rooms.RoomCode = res.room" +
+                    "where DATEDIFF(CURDATE(),checkin)<180 and roomName = " +this.RoomName+
+                    "GROUP BY roomName;";
+
+            try (Connection conn = DriverManager.getConnection(System.getenv("APP_JDBC_URL"),
+                    System.getenv("APP_JDBC_USER"),
+                    System.getenv("APP_JDBC_PW"))) {
+
+                try (Statement stmt = conn.createStatement();
+                     ResultSet rs = stmt.executeQuery(sql)) {
+                    while (rs.next()) {
+
+                        this.popularity=rs.getFloat("popular");
+
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    return 0;
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return 0;
+            }
+        }
+        return this.popularity;
     }
 }
